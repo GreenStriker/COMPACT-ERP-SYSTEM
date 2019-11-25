21024ff6816248565464dfa8d2cf8d6b366b3c26
sabbir.reza <sabbir.reza@HOER2406101641A>
2019-11-25 16:55:40 +0600

    #Content ReArrange and show image

--- vms.entity/models/Content.cs
+++ vms.entity/models/Content.cs
@@ -8,14 +8,13 @@ namespace vms.entity.models
     {
         public int ContentId { get; set; }
         public string Name { get; set; }
-        public int? ContentTypeId { get; set; }
-        public int? TransId { get; set; }
+        public int? ProductId { get; set; }
         public string Url { get; set; }
         public string Remark { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? CreatedTime { get; set; }
         public bool? IsActive { get; set; }
 
-        public virtual Contenttype ContentType { get; set; }
+        public virtual Product Product { get; set; }
     }
 }
--- vms.entity/models/Contenttype.cs
+++ vms.entity/models/Contenttype.cs
@@ -6,16 +6,9 @@ namespace vms.entity.models
 {
     public partial class Contenttype
     {
-        public Contenttype()
-        {
-            Contents = new HashSet<Content>();
-        }
-
         public int ContentTypeId { get; set; }
         public string Name { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? CreatedTime { get; set; }
-
-        public virtual ICollection<Content> Contents { get; set; }
     }
 }
