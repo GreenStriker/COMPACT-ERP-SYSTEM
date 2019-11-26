e60a7032a7b09a20ecdf27a8ff5024e54713f714
moukh rahman <moukhkst@hotmail.com>
2019-11-26 16:59:08 +0600

    payment in expence

--- vms.entity/viewModels/VmExpence.cs
+++ vms.entity/viewModels/VmExpence.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using vms.entity.models;
 
 namespace vms.entity.viewModels
 {
-    public class Vmpayment
-    {  
-       
-
+    public class VmExpence:Expence
+    {
+       // public Expence expense { get; set; }
+        //public Payment payment { get; set; }
 
     }
 }
