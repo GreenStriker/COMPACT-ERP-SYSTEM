2539aa2350d26150b442a285871298bd7241b43f
sabbir.reza <sabbir.reza@HOER2406101641A>
2019-12-05 18:17:31 +0600

    ###

--- vms.data/InventoryContext.cs
+++ vms.data/InventoryContext.cs
@@ -162,7 +162,7 @@ namespace vms.entity.models
 
             modelBuilder.Entity<Color>(entity =>
             {
-                entity.ToTable("Color");
+                entity.ToTable("Color", "dbo");
 
                 entity.Property(e => e.ColorId).HasColumnName("ColorID");
 
@@ -913,7 +913,7 @@ namespace vms.entity.models
 
             modelBuilder.Entity<Theme>(entity =>
             {
-                entity.ToTable("theme");
+                entity.ToTable("theme", "dbo");
 
                 entity.Property(e => e.ThemeId).HasColumnName("themeID");
 
