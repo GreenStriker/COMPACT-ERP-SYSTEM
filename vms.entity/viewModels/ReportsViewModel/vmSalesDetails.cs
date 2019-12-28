7c3841cb0a9c8c153c6d0060a0d29d986821e77a
moukh rahman <moukhkst@hotmail.com>
2019-12-29 00:19:50 +0600

    sales report

--- vms.entity/viewModels/ReportsViewModel/vmSalesDetails.cs
+++ vms.entity/viewModels/ReportsViewModel/vmSalesDetails.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using vms.entity.models;
 
 namespace vms.entity.viewModels.ReportsViewModel
 {
     public class vmSalesDetails
     {
-        public string InvoiceNo { get; set; }
-        public string CustomerName { get; set; }
-        public DateTime? FromDate { get; set; }
-        public DateTime? ToDate { get; set; }
+        public IEnumerable<SalesDetail> Listsale { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
     }
 }
